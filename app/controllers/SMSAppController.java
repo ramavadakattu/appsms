de5306566ec376b51c789162e60394f214fbadcb
rvadakattu <rvadakattu@ebay.com>
2012-12-18 22:26:57 +0530

    Changed code as per txtweb

--- app/controllers/SMSAppController.java
+++ app/controllers/SMSAppController.java
@@ -15,13 +15,19 @@ public class SMSAppController extends Controller {
     
     public static void getItemTitle(Long itemId)
     {
-    	Logger.debug("Itemid = %d",itemId);   	
-    	
+    	Logger.debug("Itemid = %d textweb-message = %d",itemId,params.get("txtweb-message"));   	
+    	if (itemId == null)
+    	{
+    		if (params.get("txtweb-message") != null)
+    		{
+    			itemId = Long.valueOf(params.get("txtweb-message"));
+    		}
+    	}    	
     	/**
     	 * Call eBay API and get the title
     	 */
     	String title = ProductionApiWrapper.getItemTitle(itemId);    	  	
-    	render("appsms/main.html",itemId,title);
+    	render("appsms/txtweb.html",itemId,title);
     	
     }
 
